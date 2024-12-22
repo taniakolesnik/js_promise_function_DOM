c989aaddfe85c666e293d56f01ee59e11b1d0f64
Tania Kolesnik <kolesnik@gmail.com>
2024-12-22 18:28:25 +0000

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,28 @@
+"use strict";
+function waitFor(element, eventName) {
+    const promise = new Promise((resolve, reject)=>{
+        element.addEventListener(eventName, ()=>{
+            resolve(`It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`);
+        });
+    });
+    return promise;
+}
+const printMessage = (message)=>{
+    const div = document.createElement("div");
+    div.classList.add("message");
+    div.textContent = message;
+    document.body.append(div);
+};
+const loginField = document.getElementById("login");
+const passwordField = document.getElementById("password");
+const button = document.getElementById("submit");
+waitFor(loginField, "click").then(printMessage);
+waitFor(passwordField, "click").then(printMessage);
+waitFor(button, "click").then(printMessage);
+waitFor(loginField, "input").then(printMessage);
+waitFor(passwordField, "input").then(printMessage);
+waitFor(loginField, "blur").then(printMessage);
+waitFor(passwordField, "blur").then(printMessage);
+waitFor(button, "blur").then(printMessage);
+
+//# sourceMappingURL=index.f75de5e1.js.map
